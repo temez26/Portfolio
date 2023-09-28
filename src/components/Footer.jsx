3925d401ee88769bf576a572dbd1f8172468d730
Teemu Kalmari <Teemu.Kalmari@student.lab.fi>
2023-09-28 11:50:37 +0300

    added background icons to the body and added hover logic to the icons.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,59 @@
+import React, { useState } from 'react';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Lottie from 'lottie-react';
+import Github from '../assets/github.json';
+import Linkedin from '../assets/linkedin.json';
+
 
 function Footer() {
+    const [linkedinStopped, setLinkedinStopped] = useState(false);
+    const [githubStopped, setGithubStopped] = useState(false);
+
+    const onMouseEnterLinkedin = () => {
+        setLinkedinStopped(true);
+        console.log("LinkedIn animate");
+    };
+
+    const onMouseLeaveLinkedin = () => {
+        setLinkedinStopped(false);
+        console.log("LinkedIn stop");
+    };
+
+    const onMouseEnterGithub = () => {
+        setGithubStopped(true);
+        console.log("GitHub animate");
+    };
+
+    const onMouseLeaveGithub = () => {
+        setGithubStopped(false);
+        console.log("GitHub stop");
+    };
 
     return (
         <footer>
             <div className="down">
                 <section className="socials">
-                <div >
+                    <div className="social-icons-container">
                         <a href="https://github.com/temez26" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithub} className='github' />
+                            <div className="icon-container" onMouseEnter={onMouseEnterGithub} onMouseLeave={onMouseLeaveGithub}>
+                                <Lottie animationData={Github} loop={githubStopped} />
+                            </div>
                         </a>
                         <a href="https://www.linkedin.com/in/teemu-kalmari-755469169/" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} className='linkedin' />
+                            <div className="icon-container" onMouseEnter={onMouseEnterLinkedin} onMouseLeave={onMouseLeaveLinkedin}>
+                                <Lottie animationData={Linkedin} loop={linkedinStopped} />
+                            </div>
                         </a>
                     </div>
-
                 </section>
+
                 <section className="footer-section">
                     <section> <p>Bonkkers oy</p></section>
-                
-                <section><p>Teemu Kalmari</p></section>
-               
+                    <section><p>Teemu Kalmari</p></section>
                 </section>
-             
             </div>
         </footer>
-    )
+    );
 }
-
 
 export default Footer;
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,20 +1,57 @@
+import React, { useState } from 'react';
+import Lottie from 'lottie-react';
+import Coding from '../assets/coding.json';
+import CodingApe from '../assets/codingape.json'; // Import the CodingApe animation
+import Computer from '../assets/computer.json'; // Import the Computer animation
 
 function Body() {
+    const [codingStopped, setCodingStopped] = useState(false);
+    const [codingApeStopped, setCodingApeStopped] = useState(false);
+    const [computerStopped, setComputerStopped] = useState(false);
 
-    return(
+    const onMouseEnterCoding = () => {
+        setCodingStopped(true);
+        console.log("Coding animate");
+    };
 
-        <>
+    const onMouseLeaveCoding = () => {
+        setCodingStopped(false);
+        console.log("Coding stop");
+    };
 
+    const onMouseEnterCodingApe = () => {
+        setCodingApeStopped(true);
+        console.log("CodingApe animate");
+    };
+
+    const onMouseLeaveCodingApe = () => {
+        setCodingApeStopped(false);
+        console.log("CodingApe stop");
+    };
+
+    const onMouseEnterComputer = () => {
+        setComputerStopped(true);
+        console.log("Computer animate");
+    };
+
+    const onMouseLeaveComputer = () => {
+        setComputerStopped(false);
+        console.log("Computer stop");
+    };
+
+    return (
         <div className="Body">
-
-        This is body
-
+            <div className="icon-body1" onMouseEnter={onMouseEnterCoding} onMouseLeave={onMouseLeaveCoding}>
+                <Lottie animationData={Coding} loop={codingStopped} />
+            </div>
+            <div className="icon-body2" onMouseEnter={onMouseEnterCodingApe} onMouseLeave={onMouseLeaveCodingApe}>
+                <Lottie animationData={CodingApe} loop={codingApeStopped} />
+            </div>
+            <div className="icon-body3" onMouseEnter={onMouseEnterComputer} onMouseLeave={onMouseLeaveComputer}>
+                <Lottie animationData={Computer} loop={computerStopped} />
+            </div>
         </div>
-        
-        
-        </>
-    )
+    );
 }
-
 
 export default Body;
